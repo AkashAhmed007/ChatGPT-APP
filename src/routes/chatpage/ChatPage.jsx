0f9661c4e44d1c2986c6752d254bc5c02de7bad5
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-08-09 20:21:46 +0600

    new prompt section added

--- src/routes/chatpage/ChatPage.jsx
+++ src/routes/chatpage/ChatPage.jsx
@@ -1,8 +1,34 @@
 import './chatpage.css'
-
+import NewPrompt from '../../components/newprompt/NewPrompt'
 function ChatPage() {
   return (
-    <div>ChatPage</div>
+    <div className='chatpage'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user">Test message from user</div>
+          <NewPrompt></NewPrompt>
+        </div>
+      </div>
+    </div>
   )
 }
 
