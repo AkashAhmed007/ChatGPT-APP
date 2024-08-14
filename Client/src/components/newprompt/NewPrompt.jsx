4f4f6dbc3179994690bf5ab45775e98449d5480a
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-08-14 11:26:14 +0600

    gemini AI setup

--- Client/src/components/newprompt/NewPrompt.jsx
+++ Client/src/components/newprompt/NewPrompt.jsx
@@ -3,18 +3,34 @@ import { FaLongArrowAltUp } from "react-icons/fa";
 import { useEffect, useRef, useState } from "react";
 import Upload from "../upload/Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../lib/geminiAI";
+import Markdown from 'react-markdown'
 function NewPrompt() {
   const [img, setImg] = useState({
     isLoading: false,
     error: "",
     dbData: {},
   });
+const [question,setQuestion]= useState('')
+const [answer,setAnswer]= useState('')
 const endRef = useRef(null);
 
 useEffect(() => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
-  }, []);
-  
+  }, [question,answer,img.dbData]);
+
+const add = async (text)=>{
+  setQuestion(text)
+  const result = await model.generateContent(text);
+  const response = await result.response;
+   setAnswer(response.text());
+}
+const handleSubmit = async (e)=>{
+  e.preventDefault()
+  const text = e.target.text.value;
+  if(!text) return;
+  add(text)
+}
   return (
     <>
       {img.isLoading && <div>Loading...</div>}
@@ -25,10 +41,12 @@ useEffect(() => {
           width="200px"
         />
       )}
+        {question && <div className="message user">{question}</div>}
+        {answer && <div className="message"><Markdown>{answer}</Markdown></div>}
       <div className="endchat" ref={endRef}></div>
-      <form className="newForm">
+      <form className="newForm" onSubmit={handleSubmit}>
         <Upload setImg={setImg}></Upload>
-        <input type="text" placeholder="Ask me Anything.." />
+        <input type="text" name="text" placeholder="Ask me Anything.." />
         <button>
           <FaLongArrowAltUp className="icon" />
         </button>
